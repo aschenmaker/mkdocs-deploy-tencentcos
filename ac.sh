4342a4bc6daf6846ad1432ee9362c9e0646dccfc
costa <aschen@cug.edu.cn>
2021-09-05 13:19:59 +0800

    fix vars

--- ac.sh
+++ ac.sh
@@ -21,7 +21,7 @@ fi
 
 mkdocs build  --config-file "${CONFIG_FILE}"
 
-UPLOAD_ARGS="delete -r -f / && upload -r ./site/ /"
+UPLOAD_ARGS="delete -r -f / && upload -r ${GITHUB_WORKSPACE}/site/ /"
 
 if [ -z "$SECRET_ID" ]; then
   print_info '::error::Required SecretId parameter'
